a7d86943c3c7a455706e4ec1522a5fc653fc52d3
oba <thevetdoctor@gmail.com>
2019-03-28 12:00:02 +0100

    [finishes #164894267]-landing-page

---
+++ ui/main.js
@@ -0,0 +1,17 @@
+// console.log('Banka');
+
+// const tab = document.querySelector('.menu-tab');
+// const list = document.querySelector('.menu-list');
+
+// const menuOpen = () => {
+//     list.style.display = 'block';
+//     tab.style.display = 'none';
+// }
+
+// const menuClose = () => {
+//     list.style.display = 'none';
+//     tab.style.display = 'block';
+// }
+
+// tab.addEventListener('click', menuOpen);
+// list.addEventListener('click', menuClose);
