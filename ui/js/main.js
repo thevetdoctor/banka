34f827e1cfde68315bcaee692d1c04f806236c99
oba <thevetdoctor@gmail.com>
2019-03-29 11:53:08 +0100

    update landing page

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,17 +1,17 @@
-// console.log('Banka');
+console.log('Banka');
 
-// const tab = document.querySelector('.menu-tab');
-// const list = document.querySelector('.menu-list');
+const tab = document.querySelector('.menu-tab');
+const list = document.querySelector('.menu-list');
 
-// const menuOpen = () => {
-//     list.style.display = 'block';
-//     tab.style.display = 'none';
-// }
+const menuOpen = () => {
+    list.style.display = 'block';
+    tab.style.display = 'none';
+}
 
-// const menuClose = () => {
-//     list.style.display = 'none';
-//     tab.style.display = 'block';
-// }
+const menuClose = () => {
+    list.style.display = 'none';
+    tab.style.display = 'block';
+}
 
-// tab.addEventListener('click', menuOpen);
-// list.addEventListener('click', menuClose);
+tab.addEventListener('click', menuOpen);
+list.addEventListener('click', menuClose);
