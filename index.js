5416e240ae563de001c72552e11c904e3f5d1863
oba <thevetdoctor@gmail.com>
2019-04-08 11:56:24 +0100

    [finishes #165174303]server-setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+//   res.end('<h1>Welcome to Banka</h1><h4>...financial transactions at your convenience!</h4>');
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log('server started now');
+});
+
+module.exports = app;
