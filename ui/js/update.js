abc906a32cda8fefcde71c02d67da917aa8b22c6
oba <thevetdoctor@gmail.com>
2019-04-08 09:04:32 +0100

    pre-server-setup ui update

--- ui/js/update.js
+++ ui/js/update.js
@@ -1,5 +1,22 @@
 console.log('Account Credit & Debit');
 
+const creditBtn = document.getElementById('#credit-btn');
+const debitBtn = document.getElementById('#debit-btn');
+const credit = document.getElementById('#credit');
+const debit = document.getElementById('#debit');
+
+// credit.style.display = 'none';
+
+
+const goToCredit = () => {
+    debit.style.display = 'none';
+    credit.style.display = 'block';
+}
+
+console.log(creditBtn);
+console.log(debitBtn);
+creditBtn.addEventListener('click', goToCredit);
+
 
 
 const creditAccount = () => {
