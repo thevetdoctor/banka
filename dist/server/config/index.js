f6504605b4019231d75bb34b6ef6b8bfb111d472
oba <thevetdoctor@gmail.com>
2019-04-21 10:39:15 +0100

    chore(database-setup): set up database
    set up postgresql database to persist data
    
    [Finishes #165472577]

---
+++ dist/server/config/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var config = {
+  host: 'localhost',
+  user: 'postgres',
+  database: 'banka',
+  password: 'animalworld',
+  port: 5433
+};
+var _default = config;
+exports["default"] = _default;
--- src/server/models/users.js
+++ src/server/models/users.js
@@ -1,15 +1,16 @@
 class User {
-  constructor(email, firstName, lastName, password, sex, mobile, type = 'client', isAdmin = false, active = 'active') {
+  constructor(email, firstName, lastName, password, sex, mobile) {
     this.id = Number();
     this.email = email;
     this.firstName = firstName;
     this.lastName = lastName;
     this.password = password;
-    this.type = type;
-    this.isAdmin = isAdmin;
+    this.type = 'client';
+    this.isAdmin = false;
     this.sex = sex;
     this.mobile = mobile;
-    this.active = active;
+    this.active = false;
+    this.createdDate = new Date().toDateString();
   }
 }
 
